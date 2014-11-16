8df3d0c1030bf9de15394fc360cf157459315e31
Garret Alfert <alfert@wevelop.de>
2014-11-16 20:25:06 +0100

    Import sockethub-client.js

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 'use strict';
 
+var path = require('path');
+
 module.exports = {
-  name: 'ember-cli-sockethub'
+  name: 'ember-cli-sockethub',
+
+  treeFor: function(name) {
+    if (name !== 'vendor') { return; }
+
+    return this._unwatchedTreeGenerator(path.join(__dirname, 'node_modules'));
+  },
+
+  included: function(app) {
+    this._super.included(app);
+
+    this.app.import( 'vendor/sockethub-client/sockethub-client.js');
+  }
 };
